68b9c9ce996022e2fffa954cd0ed9c73203837a1
joseteodoro <jteodoro.dev@gmail.com>
2017-10-12 14:10:14 -0300

    adding shelve

---
+++ src/components/shelve.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Book from './book'
+
+export default (props) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{props.title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {props.books.map((book) => {
+          return <li key={book.title}><Book book={book} /></li>
+          })
+        }
+      </ol>
+    </div>
+  </div>
+)
