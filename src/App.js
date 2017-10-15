d51c72e7c15d147f556e75a660adae2ba5920c74
joseteodoro <jteodoro.dev@gmail.com>
2017-10-15 20:05:12 -0200

    initial version of search component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Shelve from './components/shelve'
+import Search from './components/search'
 
 class BooksApp extends React.Component {
   state = {
+    queryResults : [],
+    query: '',
     reading: [ {cover: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api', author: 'Harper Lee', title: 'To Kill a Mockingbird'},
                 {cover: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api', author: 'Orson Scott Card', title: 'Ender\'s Game'}],
 
@@ -23,29 +26,11 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
-  render() {
+  render () {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <Search />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
---
+++ src/components/search.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import * as BooksAPI from '../BooksAPI'
+
+class Search extends React.Component {
+
+  state = {
+    query: '',
+    queryResults : [],
+  }
+  
+  updateQuery (param) {
+    console.log(param);
+    if (param === '') {
+      this.setState({ queryResults: [] })
+    } else {
+      this.onSearch(param).then(results => {
+        this.setState({ queryResults: results })
+      })
+    }
+  }
+
+  onSearch (query) {
+      return BooksAPI.search(query, 20)
+  }
+
+
+  render () {
+    return (<div className="search-books">
+      <div className="search-books-bar">
+        <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+        <div className="search-books-input-wrapper">
+          {/*
+            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+            You can find these search terms here:
+            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+            you don't find a specific author or title. Every search is limited by search terms.
+          */}
+          <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) =>  this.updateQuery(event.target.value)}/>
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {JSON.stringify(this.state.queryResults)}
+        </ol>
+      </div>
+    </div>)
+  }
+
+
+}
+
+export default Search
