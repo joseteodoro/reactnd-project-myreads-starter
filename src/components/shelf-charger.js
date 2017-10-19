ae4b77bc51a6915051daa2441a6f0b4486fe1a39
joseteodoro <jteodoro.dev@gmail.com>
2017-10-19 09:39:16 -0200

    fix dropdown selected item

--- src/components/shelf-charger.js
+++ src/components/shelf-charger.js
@@ -3,14 +3,11 @@ import PropTypes from 'prop-types'
 
 const ShelfCharger = (props) => (
   <div className='book-shelf-changer'>
-    <select onChange={(event) => {
+    <select value={props.book.shelf} onChange={(event) => {
       props.onChangeShelf(props.book, event.target.value)
     }}>
-      <option defaultValue value='none'>Move to...</option>
-      {props.shelves.filter((item) => {
-        return item.key !== props.book.shelf
-      })
-      .map((item) => {
+      <option disabled value='none'>Move to...</option>
+      {props.shelves.map((item) => {
         return <option key={`${item.key}`} value={`${item.key}`}>{`${item.value}`}</option>
       })}
     </select>
